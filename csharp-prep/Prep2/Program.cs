bbe138c43b0be9a7dfe9e242b6155d0fedf5e606
slasherjoe <slasher2033@protonmail.com>
2023-04-20 00:30:37 +0200

    completed core req. for prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentString = Console.ReadLine();
+        int gradePercent = int.Parse(gradePercentString);
+        char letter;
+
+        if (gradePercent >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+        Console.WriteLine(letter);
+
+        // Blank line separator.
+        Console.WriteLine();
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You need at least 70% to pass this class. Better luck next time.");
+        }
     }
 }
