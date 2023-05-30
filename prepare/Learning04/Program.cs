37e2cf5a41f6bc4f88e03216b16d1d2f97d6878d
slasherjoe <slasher2033@protonmail.com>
2023-05-30 16:32:41 +0200

    learning04 task complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Joan Çerepi", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Joan Çerepi", "Fractions", "7.3", "9-11");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine();
+        
+        WritingAssignment assignment3 = new WritingAssignment(
+            "Joan Çerepi", "European History", "The Causes of World War II"
+        );
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
